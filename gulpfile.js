514b9cd9153c439bd2eb72d20a48af521c66d799
m.stein <m.stein@conterra.de>
2019-09-23 16:22:55 +0200

    Update to map.apps 4.7.2

--- gulpfile.js
+++ gulpfile.js
@@ -14,16 +14,15 @@
  * limitations under the License.
  */
 const gulp = require("gulp");
-const run_sequence = require('run-sequence');
 const mapapps = require('ct-mapapps-gulp-js');
 
 mapapps.registerTasks({
     /* A detailed description of available setting is available at https://www.npmjs.com/package/ct-mapapps-gulp-js */
     /* a list of themes inside this project */
     themes: [/*"sample-theme"*/],
-    /* state that the custom theme will be dependant from map.apps everlasting theme that provides the base css */
+    /* state that the custom theme will be dependant from map.apps everlasting theme that provides the base styles */
     hasBaseThemes: true,
-    /* state that we want to support vuetify components and therefore need the the vuetify core css*/
+    /* state that we want to support vuetify components and therefore need the the vuetify core styles*/
     hasVuetify: true,
     /*themeChangeTargets: {
         "vuetify": [
@@ -32,10 +31,10 @@ mapapps.registerTasks({
     }*/
 });
 
-gulp.task("default", function (callback) {
-    run_sequence(
+gulp.task("default",
+    gulp.series(
         "copy-resources",
         "themes-copy",
-        ["js-transpile", "themes-compile"],
-        callback);
-});
+        gulp.parallel("js-transpile", "themes-compile")
+    )
+);
