a5b0c8a8ac9f5a59bab681a07225b34935d7d113
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-07-10 17:48:02 +0200

    Update application title in localization bundles to reflect printing enhanced sample

--- src/main/js/apps/sample/nls/de/bundle.js
+++ src/main/js/apps/sample/nls/de/bundle.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 module.exports = {
-    apptitle: "DevNet Bundle Blueprint",
+    apptitle: "Printing Enhanced Sample",
     map: {
         koeln1: {
             title: "Basisdaten",
