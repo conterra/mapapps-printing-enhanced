4270e647812160c3ecc57169f8e3d02ff9cf8270
Matthias Stein <m.stein@conterra.de>
2025-11-19 17:53:31 +0100

    Refactor fetch method to use apprtFetchJson for improved data handling

--- src/main/js/bundles/dn_printingenhanced/PrintingInfosAnalyzer.js
+++ src/main/js/bundles/dn_printingenhanced/PrintingInfosAnalyzer.js
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 import apprt_when from "apprt-core/when";
-import { apprtFetch } from "apprt-fetch";
+import { apprtFetchJson } from "apprt-fetch";
 import * as geoprocessor from "@arcgis/core/rest/geoprocessor";
 
 const _printInfos = Symbol("_printInfos");
@@ -27,7 +27,7 @@ export default class PrintingInfosAnalyzer {
             return this[_printInfos];
         }
         // otherwise request them from server
-        return apprt_when(apprtFetch(url, {
+        return apprt_when(apprtFetchJson(url, {
             "query": {
                 "f": "json"
             }
